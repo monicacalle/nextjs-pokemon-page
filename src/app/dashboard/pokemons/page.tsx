d5dc39b62aa6e2d275e271cd11980130463353a1
monica <monicacalle369@gmail.com>
2023-12-27 15:46:01 +0100

    adding pokemon api request

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,3 +1,11 @@
+const getPokemons = async (limit = 20, offset = 0) => {
+  const data = await fetch(
+    `http://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  ).then((res) => res.json);
+
+  return data;
+};
+
 export default function PokemonsPage() {
   return (
     <div>
