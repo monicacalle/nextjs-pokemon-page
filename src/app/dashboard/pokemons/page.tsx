fab3d5e7fcfa9a766dd41c8a8e4f5c462468d7bd
monica <monicacalle369@gmail.com>
2023-12-27 18:23:48 +0100

    adding pokemon api request

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,15 +1,38 @@
-const getPokemons = async (limit = 20, offset = 0) => {
-  const data = await fetch(
+import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import Image from "next/image";
+
+const getPokemons = async (
+  limit = 20,
+  offset = 0
+): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(
     `http://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
   ).then((res) => res.json);
 
-  return data;
+  const pokemons = data.results.map((pokemon) => ({
+    id: pokemon.url.split("/").at(-2)!,
+    name: pokemon.name,
+  }));
+
+  return pokemons;
 };
 
-export default function PokemonsPage() {
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons(151);
+
   return (
-    <div>
-      <h1>Hello Page</h1>
+    <div className="flex flex-col">
+      <div className="flex flex-wrap gap-10 items-center justify-center">
+        {pokemons.map((pokemon) => (
+          <Image
+            key={pokemon.id}
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+            width={100}
+            height={100}
+            alt={pokemon.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
