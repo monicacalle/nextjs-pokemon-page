e19fa4812f28f5b6a37aec0b11cb80ec6dde92da
monica <monicacalle369@gmail.com>
2023-12-28 17:19:45 +0100

    add a new error page to handle them

--- src/app/pokemons/components/PokemonGrid.tsx
+++ src/app/pokemons/components/PokemonGrid.tsx
@@ -9,14 +9,7 @@ export const PokemonGrid = ({ pokemons }: Props) => {
   return (
     <div className="flex flex-wrap gap-10 items-center justify-center">
       {pokemons.map((pokemon) => (
-        <PokemonCard key={pokemon.id} />
-        // <Image
-        //   key={pokemon.id}
-        //   src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
-        //   width={100}
-        //   height={100}
-        //   alt={pokemon.name}
-        // />
+        <PokemonCard key={pokemon.id} pokemon={pokemon} />
       ))}
     </div>
   );
