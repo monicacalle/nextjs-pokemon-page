e2f278078e738261939e55474c1235e93c269874
monica <monicacalle369@gmail.com>
2023-12-29 19:52:36 +0100

    adding error page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+    ],
   },
 };
 
--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -27,7 +27,7 @@ export const PokemonCard = ({ pokemon }: Props) => {
           </p>
           <div className="mt-5">
             <Link
-              href={`dashboard/pokemon/${id}`}
+              href={`pokemon/${id}`}
               className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100"
             >
               More information
